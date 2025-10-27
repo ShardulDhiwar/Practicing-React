dde9e0fb0c4843ee23502d871588a3b4a1647585
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-10-27 13:18:39 +0530

    Adding ReactHooks file

---
+++ ReactHooks/hook-example/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Introduction to Hooks</h1>
+      <h3>Counter: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </>
+  )
+}
+
+export default App
