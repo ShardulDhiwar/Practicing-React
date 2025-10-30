d91cd7dcd2f1473e773bb8e5ec5dc87e0f1ad8f7
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-10-30 17:27:59 +0530

    Added new Page

--- react-router-demo/src/App.jsx
+++ react-router-demo/src/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Submit from './pages/Submit';
 function App() {
 
   return (
     <>
-      <div>
-        <nav>
-        
+      <Router>
+        <nav className="p-5 gap-5 bg-gray-200 flex justify-between items-center">
+          <h1 className="text-3xl">React Router Demo</h1>
+          <ul className="flex justify-center gap-25 mx-100">
+            <li className="hover:text-cyan-800 hover:underline">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="hover:text-cyan-800 hover:underline">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="hover:text-cyan-800 hover:underline">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="hover:text-cyan-800 hover:underline">
+              <Link to="/submit">Submit</Link>
+            </li>
+          </ul>
         </nav>
-      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/submit" element={<Submit />} />
+        </Routes>
+      </Router>
     </>
-    
   );
 }
 
