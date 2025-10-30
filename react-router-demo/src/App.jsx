a4ee93c597b6f5c3ed771f6f5941a8aaa4030003
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2025-10-30 16:47:08 +0530

    Learning React Router Concept

---
+++ react-router-demo/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+function App() {
+
+  return (
+    <>
+      <div>
+        <nav>
+        
+        </nav>
+      </div>
+    </>
+    
+  );
+}
+
+export default App
